cb2ee858ad1f34f671a6b1159346f0fa84be450d
medouassim <55862605+medouassim@users.noreply.github.com>
2021-02-22 01:37:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var labels = document.querySelectorAll('label');
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter, idx) => `<span style='transition-delay: ${idx * 30}ms'>${letter}</span>`)
+        .join('');
+})
+
